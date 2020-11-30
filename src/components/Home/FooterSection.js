08d9a86b40393948108296841d52ce7bbb83f532
Yhunie <yhuniegaming@gmail.com>
2020-11-30 13:33:56 +0100

    Second section done

--- src/components/Home/FooterSection.js
+++ src/components/Home/FooterSection.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import woman_in_sunglasses from "./../../images/woman_in_sunglasses.jpg"
+
+//<span>Photo by <a href="https://unsplash.com/@heinrich_boll?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Mohammad Rezaie</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
 
 export default function FooterSection() {
      return (
