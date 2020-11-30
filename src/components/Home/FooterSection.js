2cf490158f2965785ffc0490748378bc1e667c08
Yhunie <yhuniegaming@gmail.com>
2020-11-30 14:46:48 +0100

    Footer done

--- src/components/Home/FooterSection.js
+++ src/components/Home/FooterSection.js
@@ -1,12 +1,42 @@
 import React from 'react'
+import { Link } from "react-router-dom"
 import woman_in_sunglasses from "./../../images/woman_in_sunglasses.jpg"
+import watch from "./../../images/watch.jpg"
+import woman_portrait from "./../../images/woman_portrait.jpg"
+import pen from "./../../images/pen.jpg"
+import "./../../styles/links.scss"
+import "./../../styles/footer.scss"
+
 
 //<span>Photo by <a href="https://unsplash.com/@heinrich_boll?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Mohammad Rezaie</a> on <a href="https://unsplash.com/?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span>
 
 export default function FooterSection() {
-     return (
-          <div>
+   return (
+      <div id="footer">
 
-          </div>
-     )
+         <div id="footer-top">
+            <div id="footer-text-container">
+               <h1>the list</h1>
+               <h2>Don't miss a thing</h2>
+               <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget</p>
+               <Link to="/contact">Sign me up!</Link>
+            </div>
+
+            <div id="footer-line"></div>
+
+            <div id="footer-photos">
+               <img src={woman_in_sunglasses} alt="woman_in_sunglasses" />
+               <img src={watch} alt="watch" />
+               <img src={woman_portrait} alt="woman_portrait" />
+               <img src={pen} alt="pen" />
+            </div>
+         </div>
+
+
+         <div id="footer-bottom">
+            <div></div>
+         </div>
+
+      </div>
+   )
 }
