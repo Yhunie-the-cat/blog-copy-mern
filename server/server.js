c5fbefc2ab5f2794038e6d3b915703bfd6243f29
Yhunie <yhuniegaming@gmail.com>
2020-12-10 11:53:29 +0100

    Create server

--- server/server.js
+++ server/server.js
@@ -6,23 +6,46 @@
 // https://www.youtube.com/watch?v=fgTGADljAeg
 // https://www.youtube.com/watch?v=5yTazHkDR4o&list=PLillGF-RfqbbiTGgA77tGO426V3hRF9iE&index=2
 
+//mongodb+srv://<username>:<password>@blog.gdy6j.mongodb.net/<dbname>?retryWrites=true&w=majority
+
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-require("dotenv").config();
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
+//  Middleware
 app.use(cors());
 app.use(express.json());
+//--
+
+const uri = process.env.ATLAS_URI;
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully!");
+});
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(error.message));
 
 app.get("/", (req, res) => {
   console.log("test");
   res.send("Homepage");
 });
 
-app.listen(PORT, () =>
+//  Start server
+/*app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
-);
+);*/
