481c63069eabccc8f577705cf524768a86c6e3f1
Yhunie <yhuniegaming@gmail.com>
2020-12-16 23:54:32 +0100

    server cleanup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,3 @@
-// https://www.youtube.com/watch?v=vjf774RKrLc
-// https://www.youtube.com/watch?v=qj2oDkvc4dQ&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM&index=5
-// https://www.youtube.com/watch?v=7CqJlxBYj-M    //!This
-// https://www.youtube.com/watch?v=ngc9gnGgUdA    //!This
-// https://www.youtube.com/watch?v=l8WPWK9mS5M
-// https://www.youtube.com/watch?v=fgTGADljAeg
-// https://www.youtube.com/watch?v=5yTazHkDR4o&list=PLillGF-RfqbbiTGgA77tGO426V3hRF9iE&index=2
-
-//mongodb+srv://<username>:<password>@blog.gdy6j.mongodb.net/<dbname>?retryWrites=true&w=majority
-
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
@@ -42,8 +32,3 @@ mongoose
       )
    )
    .catch((error) => console.log(error.message));
-
-//  Start server
-/*app.listen(PORT, () =>
-  console.log(`Server running on port: http://localhost:${PORT}`)
-);*/
