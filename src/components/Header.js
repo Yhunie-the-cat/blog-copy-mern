e19fe15551cac33a9a86eea489a2f15d369d4d33
Yhunie <yhuniegaming@gmail.com>
2020-12-19 15:48:56 +0100

    Burger menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Header() {
-  return (
-    <div id="header">
-      <nav>
-        <ul>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/photos">Photos</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/contact">Contact</Link>
-        </ul>
-      </nav>
-      <h1>Kate</h1>
-    </div>
-  );
+   const [isClicked, setIsClicked] = useState(false);
+   const [navLinks, setNavLinks] = useState([]);
+   const burgerRef = useRef();
+   const menuRef = useRef();
+
+   useEffect(() => {
+      setNavLinks(Object.values(menuRef.current.children));
+   }, [menuRef]);
+
+   const handleClick = () => {
+      setIsClicked((state) => !state);
+      navLinks.forEach((link, index) => {
+         if (link.style.animation) {
+            link.style.animation = "";
+         } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${
+               index / 7 + 0.5
+            }s`;
+         }
+      });
+   };
+
+   return (
+      <div id="header">
+         <div
+            id="burger"
+            className={isClicked ? "toggle" : ""}
+            onClick={handleClick}
+            ref={burgerRef}
+         >
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
+         </div>
+         <nav>
+            <ul ref={menuRef} className={isClicked ? "nav-active" : ""}>
+               <Link to="/">Home</Link>
+               <Link to="/about">About</Link>
+               <Link to="/photos">Photos</Link>
+               <Link to="/blog">Blog</Link>
+               <Link to="/contact">Contact</Link>
+            </ul>
+         </nav>
+         <h1>Kate</h1>
+      </div>
+   );
 }
